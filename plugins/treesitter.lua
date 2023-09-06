a0e73c04d8a7d87e00babc484fd048a408ac3255
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-09-06 17:23:23 +0900

    Add a commemnt

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts={
+    -- Highly recommended to install treesitter-cli on your machine to use auto-install
     auto_install=true,
     autotag={
       enable=true
