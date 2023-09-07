da2fc1e52711d244190898ab9bbc946876b038a6
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-09-07 14:09:25 +0900

    Replace multicursor plugin

--- plugins/init.lua
+++ plugins/init.lua
@@ -6,22 +6,23 @@ return {
     config = function() require("catppuccin").setup {} end,
   },
   {
-    "smoka7/multicursors.nvim",
+    "mg979/vim-visual-multi",
     event = "VeryLazy",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "smoka7/hydra.nvim",
-    },
-    opts = {},
-    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-    keys = {
-      {
-        mode = { "v", "n" },
-        "<Leader>m",
-        "<cmd>MCstart<cr>",
-        desc = "Create a selection for selected text or word under the cursor",
-      },
-    },
+    config = function()
+      vim.cmd [[
+        let g:VM_leader = '_'
+
+        if !exists('g:VM_maps')
+          let g:VM_maps = {}
+        endif
+
+        let g:VM_maps["Select h"] = ''
+        let g:VM_maps["Select l"] = ''
+
+        let g:VM_maps["Add Cursor Down"]             = '_j'
+        let g:VM_maps["Add Cursor Up"]               = '_k'
+      ]]
+    end,
   },
   {
     "kylechui/nvim-surround",
