9f2dc7e3c6a51a032210ebd01f15f9602e4650d4
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-11-05 13:20:38 +0900

    Run prettier on insert-leave

--- autocmds/auto-format/init.lua
+++ autocmds/auto-format/init.lua
@@ -10,10 +10,17 @@ end ]]
 
 -- How to get bufnr?
 --
-local augroup = vim.api.nvim_create_augroup("FormatOnSave", {})
-vim.api.nvim_clear_autocmds({ group = augroup })
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-	pattern = { "*.lua" },
-	group = augroup,
-	command = "! stylua %",
+-- local augroup = vim.api.nvim_create_augroup("FormatOnSave", {})
+-- vim.api.nvim_clear_autocmds { group = augroup }
+-- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+--   pattern = { "*.lua" },
+--   group = augroup,
+--   command = "! stylua %",
+-- })
+vim.api.nvim_create_augroup("format_on_leave", { clear = true })
+vim.api.nvim_create_autocmd("InsertLeave", {
+  desc = "Auto format before leave from insert-mode",
+  group = "format_on_leave",
+  pattern = { "*.tsx", "*.ts", "*.js", "*.jsx", "*.html", "*.md", "*.mdx", "*.yml" },
+  command = "%!bunx prettier --stdin-filepath %",
 })
--- autocmds/init.lua
+++ autocmds/init.lua
@@ -4,4 +4,4 @@ require "user.autocmds.autowrite"
 require "user.autocmds.highlight-yank"
 -- require('autocmds.folding_with_treesitter')
 -- require('autocmds.format-after-insertleave')
--- require('autocmds.format-on-save')
+require("user.autocmds.auto-format")
