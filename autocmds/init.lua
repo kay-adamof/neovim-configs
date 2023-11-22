affdada574a12b3cb78f02db9df2b75bf9421d7e
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-11-22 10:13:43 +0900

    Make code unfold

--- autocmds/init.lua
+++ autocmds/init.lua
@@ -2,6 +2,6 @@ require "user.autocmds.autowrite"
 -- require "user.autocmds.Export_Terminal_to_File"
 -- require('autocmds.auto-format')
 require "user.autocmds.highlight-yank"
--- require('autocmds.folding_with_treesitter')
+require 'user.autocmds.folding_with_treesitter'
 -- require('autocmds.format-after-insertleave')
 -- require("user.autocmds.auto-format")
